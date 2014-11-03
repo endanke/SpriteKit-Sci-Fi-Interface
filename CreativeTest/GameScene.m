224b42c40a4c80fea1ba2db998bbd04258715fb2
Daniel Eke <endanke@gmail.com>
2014-11-03 11:20:18 +0100

    Initial commit, rebuilding

--- CreativeTest/GameScene.m
+++ CreativeTest/GameScene.m
@@ -8,42 +8,147 @@
 
 #import "GameScene.h"
 
+@interface GameScene() <SKPhysicsContactDelegate>
+
+// General objects
+@property (nonatomic) NSMutableArray *wayPoints;
+
+// General nodes
+@property (nonatomic) SKSpriteNode* testNode;
+@property (nonatomic) SKShapeNode* circleDots;
+@property (nonatomic) SKEffectNode* mainEffect;
+@property (nonatomic) SKShapeNode* canvasNode;
+
+// Physic mutators
+@property (nonatomic) SKFieldNode* springField;
+@property (nonatomic) SKFieldNode* noiseField;
+
+@end
+
 @implementation GameScene
 
+float rotateCounter = 0.1;
+float rotateScale = 0.0;
+
 -(void)didMoveToView:(SKView *)view {
-    /* Setup your scene here */
-    SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+    self.backgroundColor = UIColor.whiteColor;
+    self.wayPoints = [NSMutableArray array];
     
-    myLabel.text = @"Hello, World!";
-    myLabel.fontSize = 65;
-    myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                   CGRectGetMidY(self.frame));
+    self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
+    self.physicsWorld.gravity = CGVectorMake(0.0, 0.0);
+    self.physicsWorld.contactDelegate = self;
     
-    [self addChild:myLabel];
+    self.testNode = [[SKSpriteNode alloc] initWithTexture:nil color:UIColor.redColor size:CGSizeMake(30, 30)];
+    self.testNode.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:self.testNode.size];
+    self.testNode.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame)-40);
+    [self addChild:self.testNode];
+    
+    self.circleDots = [SKShapeNode node];
+    
+    UIBezierPath *_path=[UIBezierPath bezierPathWithOvalInRect:CGRectMake(-50, -50, 100, 100)];
+    
+    CGFloat pattern[2];
+    pattern[0] = 0.0;
+    pattern[1] = 20.0;
+    CGPathRef dashed =
+    CGPathCreateCopyByDashingPath([_path CGPath],
+                                  NULL,
+                                  0,
+                                  pattern,
+                                  2);
+    
+    self.circleDots.path = dashed;
+    self.circleDots.strokeColor = UIColor.grayColor;
+    self.circleDots.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
+    self.circleDots.lineCap = kCGLineCapRound;
+    self.circleDots.lineWidth = 1.0;
+    
+    CGPathRelease(dashed);
+    
+    [self addChild:self.circleDots];
+    
+    self.springField = [SKFieldNode springField];
+    self.springField.strength = 1.0;
+    self.springField.falloff = -1.0;
+    
+    self.noiseField = [SKFieldNode noiseFieldWithSmoothness:1.0 animationSpeed:1.0];
+    
+    self.springField.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
+    self.noiseField.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
+    
+    [self addChild:self.springField];
+    [self addChild:self.noiseField];
+    
+    self.canvasNode = [SKShapeNode node];
+    self.canvasNode.strokeColor = [UIColor grayColor];
+    self.canvasNode.fillColor = nil;
+    self.canvasNode.position = CGPointMake(0,0);
+    
+    [self addChild:self.canvasNode];
 }
 
+CGPathRef CGPathCreateCopyByDashingPath(
+                                        CGPathRef path,
+                                        const CGAffineTransform *transform,
+                                        CGFloat phase,
+                                        const CGFloat *lengths,
+                                        size_t count
+                                        );
+
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    /* Called when a touch begins */
-    
     for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
         
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.xScale = 0.5;
-        sprite.yScale = 0.5;
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
     }
 }
 
 -(void)update:(CFTimeInterval)currentTime {
-    /* Called before each frame is rendered */
+    rotateScale = sinf(rotateCounter);
+    rotateCounter += 0.01;
+    self.circleDots.zRotation = M_PI/rotateScale;
+    
+    [self.wayPoints addObject:[NSValue valueWithCGPoint:self.testNode.position]];
+    
+    [self drawLines];
+}
+
+- (CGPathRef)createPathToMove {
+    //1
+    CGMutablePathRef ref = CGPathCreateMutable();
+    
+    //2
+    for(int i = 0; i < [self.wayPoints count]; ++i) {
+        CGPoint p = [self.wayPoints[i] CGPointValue];
+        p = [self.scene convertPointToView:p];
+        //3
+        if(i == 0) {
+            CGPathMoveToPoint(ref, NULL, p.x, p.y);
+        } else {
+            CGPathAddLineToPoint(ref, NULL, p.x, p.y);
+        }
+    }
+    
+    return ref;
+}
+
+- (void)drawLines {
+    NSMutableArray *temp = [NSMutableArray array];
+    for(CALayer *layer in self.view.layer.sublayers) {
+        if([layer.name isEqualToString:@"line"]) {
+            [temp addObject:layer];
+        }
+    }
+    
+    [temp makeObjectsPerformSelector:@selector(removeFromSuperlayer)];
+    
+    CAShapeLayer *lineLayer = [CAShapeLayer layer];
+    lineLayer.name = @"line";
+    lineLayer.strokeColor = [UIColor grayColor].CGColor;
+    lineLayer.fillColor = nil;
+    
+    CGPathRef path = [self createPathToMove];
+    self.canvasNode.path = path;
+    CGPathRelease(path);
+//    [self.view.layer addSublayer:lineLayer];
 }
 
 @end
--- CreativeTest/GameViewController.m
+++ CreativeTest/GameViewController.m
@@ -9,25 +9,6 @@
 #import "GameViewController.h"
 #import "GameScene.h"
 
-@implementation SKScene (Unarchive)
-
-+ (instancetype)unarchiveFromFile:(NSString *)file {
-    /* Retrieve scene file path from the application bundle */
-    NSString *nodePath = [[NSBundle mainBundle] pathForResource:file ofType:@"sks"];
-    /* Unarchive the file to an SKScene object */
-    NSData *data = [NSData dataWithContentsOfFile:nodePath
-                                          options:NSDataReadingMappedIfSafe
-                                            error:nil];
-    NSKeyedUnarchiver *arch = [[NSKeyedUnarchiver alloc] initForReadingWithData:data];
-    [arch setClass:self forClassName:@"SKScene"];
-    SKScene *scene = [arch decodeObjectForKey:NSKeyedArchiveRootObjectKey];
-    [arch finishDecoding];
-    
-    return scene;
-}
-
-@end
-
 @implementation GameViewController
 
 - (void)viewDidLoad
@@ -42,7 +23,7 @@
     skView.ignoresSiblingOrder = YES;
     
     // Create and configure the scene.
-    GameScene *scene = [GameScene unarchiveFromFile:@"GameScene"];
+    GameScene *scene = [[GameScene alloc] initWithSize:skView.bounds.size];
     scene.scaleMode = SKSceneScaleModeAspectFill;
     
     // Present the scene.
